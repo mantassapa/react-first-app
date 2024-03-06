e0acbe3eed55e3172eb23fb2276355e37c9d3d23
daivakosinskaite <daiva.kosinskaite@gmail.com>
2024-03-06 10:00:09 +0200

    first commit

---
+++ first-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import styles from "./styles/style.module.css"
+import classname from "classnames"
+
+function App() {
+  let number
+  return (
+    <div //style={{color:number===0?"red":"blue"}} 
+    className={classname(styles.newStyle, styles.newStyle1, "myClass")}>
+      {/* <p>Labas</p> */}
+      <p>Labas du</p>
+    </div>
+  );
+}
+
+export default App;
